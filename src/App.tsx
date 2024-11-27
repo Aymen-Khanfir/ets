c80a3f741f0579a51becb2d76dc303f0e19e65ef
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-11-27 16:27:24 +0100

    feat: :heavy_plus_sign: add shadcn and new eslint plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { toast } from 'sonner';
+
+import { ThemeProvider } from '@/context/theme-context.tsx';
+
+import { ModeToggle } from '@/components/mode-toggle.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import { Toaster } from '@/components/ui/sonner.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <div className='flex justify-between p-6'>
+        <Button onClick={() => toast.error('Event has been created.')}>
+          Open Toast
+        </Button>
+        <ModeToggle />
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Toaster />
+    </ThemeProvider>
   );
 }
 
